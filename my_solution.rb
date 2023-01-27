3f9d587003adf7839a50942c17d57fd3e2e0f5c4
david63011 <david630@gmail.com>
2023-01-27 14:00:32 -0300

    add

--- my_solution.rb
+++ my_solution.rb
@@ -4,9 +4,8 @@ require_relative 'my_list'
 list = MyList.new(1, 2, 3, 4, 5)
 
 
-puts (list.all? { |e| e < 5 })
-puts (list.all? { |e| e > 5 })
-puts (list.any? { |e| e == 2 })
-puts (list.any? { |e| e == 5 })
-puts (list.filter { |e| e.even? })
-
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+list.filter(&:even?)
