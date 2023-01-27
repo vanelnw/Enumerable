30e1bc99e5234a2dbe16dc0e1d23760edea78913
david63011 <david630@gmail.com>
2023-01-27 13:50:45 -0300

    add

--- my_solution.rb
+++ my_solution.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'my_list'
 
 
@@ -10,6 +8,4 @@ puts list.all? { |e| e < 5 }
 puts list.all? { |e| e > 5 }
 puts list.any? { |e| e == 2 }
 puts list.any? { |e| e == 5 }
-puts list.filter { |e| e.even? }
-
-
+puts list.filter(&:even?)
