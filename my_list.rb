6550db8e39f69797b12bd99064c1e218a2189e5a
david63011 <david630@gmail.com>
2023-01-27 14:14:58 -0300

    add

--- my_list.rb
+++ my_list.rb
@@ -1,29 +1,15 @@
-# frozen_string_literal: true
+
 
 require_relative 'my_enumerable'
 
 class MyList
-<<<<<<< HEAD
   include MyEnumerable
 
   def initialize(*item)
     @list = item
   end
 
-  def each(&block)
-    # yield(@list)
-    @list.each(&block)
+  def each
+    yield(@list)
   end
-=======
-
-    include MyEnumerable
- def initialize(item)
-      @list = item
-  end
-
-    def each
-     yield(@list)
-     #@list.each { |item| yield item }
-    end
->>>>>>> 75a31e6cb8cb2156af485898d92a54bf20b3c2ed
 end
