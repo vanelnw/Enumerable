c0f486c7f66ca9f6d8764b9029109dfd273b0a34
david63011 <david630@gmail.com>
2023-01-27 13:56:53 -0300

    add lintors

--- my_solution.rb
+++ my_solution.rb
@@ -4,8 +4,9 @@ require_relative 'my_list'
 list = MyList.new(1, 2, 3, 4, 5)
 
 
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-puts list.filter(&:even?)
+puts (list.all? { |e| e < 5 })
+puts (list.all? { |e| e > 5 })
+puts (list.any? { |e| e == 2 })
+puts (list.any? { |e| e == 5 })
+puts (list.filter { |e| e.even? })
+
